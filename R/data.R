0c409572f17f830eea24be242516e4810fbdf656
DivadNojnarg <dgranjon@ymail.com>
2023-10-05 15:40:55 +0200

    update datasets

--- R/data.R
+++ R/data.R
@@ -1,17 +1,58 @@
-#' CDISC clinical data
+#' Demographic data
 #'
 #' TBD
 #'
 #' @format ## `who`
-#' A list of tibbles:
+#' A tibble with `r nrow(demo)` rows and `r ncol(demo)` columns:
 #' \describe{
-#'   \item{demo}{Demographic data}
-#'   \item{expo}{Exposure data}
-#'   \item{lab}{Laboratory data}
-#'   \item{ae}{Adverse event data}
+#'   \item{STUDYID}{Study ID}
+#'   \item{DOMAIN}{Data domain}
+#'   ...
 #' }
-#' @source <https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations>
-"raw_data"
+#' @source <https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt>
+"demo"
+
+#' Exposure data
+#'
+#' TBD
+#'
+#' @format ## `who`
+#' A tibble with `r nrow(exposure)` rows and `r ncol(exposure)` columns:
+#' \describe{
+#'   \item{STUDYID}{Study ID}
+#'   \item{DOMAIN}{Data domain}
+#'   ...
+#' }
+#' @source <https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt>
+"exposure"
+
+#' Laboratory data
+#'
+#' TBD
+#'
+#' @format ## `who`
+#' A tibble with `r nrow(lab)` rows and `r ncol(lab)` columns:
+#' \describe{
+#'   \item{STUDYID}{Study ID}
+#'   \item{DOMAIN}{Data domain}
+#'   ...
+#' }
+#' @source <https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt>
+"lab"
+
+#' Adverse event data
+#'
+#' TBD
+#'
+#' @format ## `who`
+#' A tibble with `r nrow(ae)` rows and `r ncol(ae)` columns:
+#' \describe{
+#'   \item{STUDYID}{Study ID}
+#'   \item{DOMAIN}{Data domain}
+#'   ...
+#' }
+#' @source <https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt>
+"ae"
 
 #' Merged data
 #'
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,15 +1,15 @@
 ## code to prepare `DATASET` dataset goes here
-data_paths <- list(
-  demo = "https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt",
-  expo  = "https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt",
-  lab = "https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt",
-  ae = "https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt"
-)
+demo <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt")
+exposure <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt")
+lab <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt")
+ae <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt")
 
-raw_data <- lapply(data_paths, haven::read_xpt)
-merged_data <- raw_data$lab |>
+merged_data <- lab |>
   # Perform inner join on STUDYID and USUBJID
-  dplyr::inner_join(raw_data$demo, by = c("STUDYID", "USUBJID"))
+  dplyr::inner_join(demo, by = c("STUDYID", "USUBJID"))
 
-usethis::use_data(raw_data, overwrite = TRUE)
+usethis::use_data(demo, overwrite = TRUE)
+usethis::use_data(exposure, overwrite = TRUE)
+usethis::use_data(lab, overwrite = TRUE)
+usethis::use_data(ae, overwrite = TRUE)
 usethis::use_data(merged_data, overwrite = TRUE)
