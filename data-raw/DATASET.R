39096adc2e6540490d9d19f4b5b3cd87d93888a6
DivadNojnarg <dgranjon@ymail.com>
2023-10-12 22:47:54 +0200

    libs

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,14 @@
 ## code to prepare `DATASET` dataset goes here
-demo <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt")
-exposure <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt")
-lab <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt")
-ae <- haven::read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt")
+library(dplyr)
+library(haven)
+demo <- read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/dm.xpt")
+exposure <- read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ex.xpt")
+lab <- read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/lb.xpt")
+ae <- read_xpt("https://github.com/cdisc-org/sdtm-adam-pilot-project/raw/master/updated-pilot-submission-package/900172/m5/datasets/cdiscpilot01/tabulations/sdtm/ae.xpt")
 
 merged_data <- lab |>
   # Perform inner join on STUDYID and USUBJID
-  dplyr::inner_join(demo, by = c("STUDYID", "USUBJID"))
+  inner_join(demo, by = c("STUDYID", "USUBJID"))
 
 usethis::use_data(demo, overwrite = TRUE)
 usethis::use_data(exposure, overwrite = TRUE)
